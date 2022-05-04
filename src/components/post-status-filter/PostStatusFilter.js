fa8463b49575f18da0d045473764ef8f602ac835
Ziggy-Strdst <vadimpolovinko89@gmail.com>
2022-05-05 00:41:50 +0300

    FINAL 1.0

--- src/components/post-status-filter/PostStatusFilter.js
+++ src/components/post-status-filter/PostStatusFilter.js
@@ -1,16 +1,30 @@
+import React from "react";
 import "./PostStatusFilter.css";
 
-const PostStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button" className="btn btn-info">
-        Все
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Понравилось
-      </button>
-    </div>
-  );
-};
-
-export default PostStatusFilter;
+export default class PostStatusFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buttons = [
+      { name: "all", label: "Все" },
+      { name: "like", label: "Понравилось" },
+    ];
+  }
+  render() {
+    const { filter, onFilterSelect } = this.props;
+    const buttons = this.buttons.map(({ name, label }) => {
+      const active = filter === name;
+      const clazz = active ? "btn-info" : "btn-outline-secondary";
+      return (
+        <button
+          key={name}
+          type="button"
+          className={`btn ${clazz}`}
+          onClick={() => onFilterSelect(name)}
+        >
+          {label}
+        </button>
+      );
+    });
+    return <div className="btn-group">{buttons}</div>;
+  }
+}
